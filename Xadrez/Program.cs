05f71f339704a5b78afccdc2e9595c58ccce1834
Guilherme Barbieri <gdbarbieri33@gmail.com>
2022-10-07 14:51:08 -0300

    Inicio Projeto do jogo de xadrez

---
+++ Xadrez/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+using xadrez;
+
+namespace Xadrez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(8,8);
+
+            tab.colocarPeca(new Torre(tab,Cor.Preto), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab,Cor.Preto), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab,Cor.Preto), new Posicao(2, 4));
+            Tela.imprimirTabuleiro(tab);
+
+            Console.ReadLine();         }
+    }
+}
---
+++ Xadrez/tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tabuleiro;
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor  { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca( Cor cor, Tabuleiro tab)
+        {
+            this.posicao = null;
+            this.cor = cor;
+            this.tab = tab;
+            this.qteMovimentos = 0; 
+        }
+    }
+}
