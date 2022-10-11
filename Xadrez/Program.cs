d0bfa16f965ed4194d82c19939c54d1dd24406f0
Guilherme Barbieri <gdbarbieri33@gmail.com>
2022-10-11 17:18:35 -0300

    Versao com o Xequemate

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -54,7 +54,7 @@ namespace Xadrez
                 }
 
                 Console.Clear();
-                Tela.imprimirTabuleiro(partida.tab);
+                Tela.imprimirPartida(partida);
             }
             catch(TabuleiroException e)
             {
