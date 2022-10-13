2e523b742633bb8a2e971d23f5657b556f058d72
Guilherme Barbieri <gdbarbieri33@gmail.com>
2022-10-13 13:31:04 -0300

    Adicionando o resto das pecas

--- Xadrez/xadrez/Dama.cs
+++ Xadrez/xadrez/Dama.cs
@@ -7,9 +7,9 @@ using tabuleiro;
 
 namespace xadrez
 {
-    class Bispo : Peca
+    class Dama : Peca
     {
-        public Bispo(Tabuleiro tab, Cor cor) : base(cor, tab)
+        public Dama(Tabuleiro tab, Cor cor) : base(cor, tab)
         {
 
         }
@@ -26,6 +26,54 @@ namespace xadrez
             bool[,] mat = new bool[tab.linhas, tab.colunas];
 
             Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.definirValores(posicao.linha - 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 1;
+            }
+
+            //abaixo
+            pos.definirValores(posicao.linha + 1, posicao.coluna);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 1;
+            }
+
+            //direita
+            pos.definirValores(posicao.linha, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna + 1;
+            }
+
+            //esquerda
+            pos.definirValores(posicao.linha, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 1;
+            }
 
             //NO
             pos.definirValores(posicao.linha - 1, posicao.coluna - 1);
@@ -79,7 +127,6 @@ namespace xadrez
 
 
 
-
             return mat;
         }
 
@@ -87,7 +134,7 @@ namespace xadrez
 
         public override string ToString()
         {
-            return "B";
+            return "T";
         }
     }
 }
