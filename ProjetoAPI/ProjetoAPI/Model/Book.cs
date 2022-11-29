8bdd3526ae340f7bcf1f791a0600116b39c8450b
Guilherme Dinizio Barbieri <101613643+gdbarbieri@users.noreply.github.com>
2022-11-29 15:43:53 -0300

    Add files via upload

---
+++ ProjetoAPI/ProjetoAPI/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoAPI.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Title { get; set; }
+    }
+}
